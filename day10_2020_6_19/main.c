26d6e2ddfb0c693402d6ce1ba2c81aa8293a2ad2
JackFly-Bit <958141774@qq.com>
2020-06-19 18:54:41 +0800

    递归+迭代方案

---
+++ day10_2020_6_19/main.c
@@ -0,0 +1,21 @@
+//函数调用
+#include "test.h"
+
+int main()
+{
+	int n = 45;
+	int ret = fib(n);
+	printf("%d, count[%d]\n", ret, count);
+
+	//int  n = 5;
+	//printf("%d! = %d\n", n,Fact(n));
+
+	//printf("请输入你的数据# ");
+	//int data = 0;
+	//scanf("%d", &data);
+	//ShowInt(data);
+
+	//printf("helllo world!\n");
+	system("pause");
+	return 0;
+}
